38f565e07c46879cca2ee5bdb5e6dea689b82bd6
matt <hotiby@gmail.com>
2012-07-31 08:30:42 -0400

    adding the rvm commands

--- railsunicorn.sh
+++ railsunicorn.sh
@@ -12,25 +12,40 @@ echo "This script will install rails, unicorn, nginx and rvm"
 echo "#############################################"
 
 
+# Ask what the name of the site
+echo -e "\n"
+echo "What is the name of the site that is being deployed?"
+read site_name
+
+# Ask what the name of the site
+echo -e "\n"
+echo "What is your public key?"
+read PUBLIC_KEY
+
 ruby_version="1.9.3"
 ruby_version_string="1.9.3-p194"
 main_path=$(cd && pwd)/railsunicorn
 log_file="$railsready_path/install.log"
-PASSWORD="password"
 HOME_BASE='/home/'
-USER="deploy"
-PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA5WD6lGUodB9SehX5E4j1bSETMfEiiLfPgoqol/qQk6okIqv3RPPgx3qjz1yM0naq3rwvs09VvGvxewTE2KZiQfJnpkaBFkt9CIdNmkoJ1ffdt53rEQCq9bpspo0cE4O9AD6D/lM+OXVkTCP6XxqAB2L4ycW65CHsTtylO57UZQ1v7200gQ/6ns4ZDKBkg1nD3GLf81Fhmw7xfgz8miD+xqz0oYRwFfJ32RisU0efNilpx2d/N/8vHcN8OkwT/S9jJRaHylhtmbD8IjUDrbUCG6WXDwWHlSVWef7YaDP3Og0hhc4BmhqIJ3o2dCJBCLfg9OQdVyvZiUlIIcIOgPU23w== hotiby@gmail.com"
+USER="deploy20"
+TIMEZONE='America/New_York'
 
+echo -e "\n"
 echo "Updating and Upgrading installed packages..."
-sudo apt-get update && sudo apt-get -y upgrade
+yes | sudo aptitude update
+yes | sudo aptitude safe-upgrade -y
 
 echo "Creating the user and adding to the www-data"
 # sudo useradd -g www-data -p ${PASSWORD} -m -d ${HOME_BASE}${USER} ${USER}
+sudo adduser ${USER}
 sudo /usr/sbin/usermod -a -G www-data ${USER}
 
 echo "Installing some of the required packages"
-sudo apt-get install build-essential git-core python-software-properties bison openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev autoconf libxslt-dev libcurl4-openssl-dev mysql-client libmysqlclient-dev -y
+yes | sudo apt-get install build-essential git-core python-software-properties bison openssl libreadline6 libreadline6-dev curl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev autoconf libxslt-dev libcurl4-openssl-dev mysql-server mysql-client libmysqlclient-dev libmysqlclient16-dev nodejs -y
 
+# sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password password your_password'
+# sudo debconf-set-selections <<< 'mysql-server-5.1 mysql-server/root_password_again password your_password'
+# sudo apt-get -y install mysql-server
 
 echo "Installing Nginx...."
 sudo add-apt-repository ppa:nginx/stable 
@@ -46,17 +61,33 @@ echo "Updaing the Locale...."
 sudo /usr/sbin/locale-gen en_US.UTF-8
 sudo /usr/sbin/update-locale LANG=en_US.UTF-8
 
-echo "Adding the authorized_keys users..."
-cat PUBLIC_KEY >> ~/.ssh/authorized_keys
+echo "Updaing the Time Zone...."
+sudo echo "${TIMEZONE}" | sudo tee /etc/timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+
+echo "Adding the authorized_keys users and permissions..."
+echo "${PUBLIC_KEY}" >> ~/.ssh/authorized_keys
 chown -R ubuntu:ubuntu ~ubuntu/.ssh
 chmod 700 ~ubuntu/.ssh
 chmod 600 ~ubuntu/.ssh/authorized_keys
 
-sudo mkdir ~deploy/.ssh
-sudo cp ~/.ssh/authorized_keys ~deploy/.ssh/authorized_keys
-sudo chown -R deploy:deploy ~deploy/.ssh
-sudo chmod 700 ~deploy/.ssh
-sudo chmod 600 ~deploy/.ssh/authorized_keys
+sudo mkdir ${HOME_BASE}${USER}/.ssh
+sudo cp ~/.ssh/authorized_keys ${HOME_BASE}${USER}/.ssh/authorized_keys
+sudo chown -R ${USER}:${USER} ${HOME_BASE}${USER}/.ssh
+sudo chmod 700 ${HOME_BASE}${USER}/.ssh
+sudo chmod 600 ${HOME_BASE}${USER}/.ssh/authorized_keys
+
+echo -e "\n=> Installing RVM the Ruby Version Manager \n"
+sudo su ${USER} -c 'bash -s stable < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer)'
+RVM_COMMAND='[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"'
+echo "${RVM_COMMAND}" | sudo su ${USER} -c 'tee -a "$HOME/.bashrc"' >/dev/null
+sudo source ${HOME_BASE}${USER}/.bashrc
+sudo su ${USER} -c 'rvm install 1.9.3'
+sudo su ${USER} -c 'rvm use --default 1.9.3'
+echo "install: --no-rdoc --no-ri" | sudo su ${USER} -c 'tee -a "$HOME/.gemrc"' >/dev/null
+echo "update: --no-rdoc --no-ri" | sudo su ${USER} -c 'tee -a "$HOME/.gemrc"' >/dev/null
+sudo su ${USER} 'gem install rails --no-rdoc --no-ri'
+sudo su ${USER} 'gem install bundler --no-rdoc --no-ri'
 
 control_c()
 {
